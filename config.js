c3bb05921879605ffa074fe6811e2d5a08d57ac8
sogdoggoth <113285903+sogdoggoth@users.noreply.github.com>
2022-09-11 19:51:32 +1000

    Initial commit of files

---
+++ config.js
@@ -0,0 +1,17 @@
+const config = {
+    vertical:{
+        frequency:1,
+        distance:1,
+        timing_offset:1,
+        axis_offset:0,
+    },
+    horizontal:{
+        frequency: 1,
+        distance: 1,
+        timing_offset:0,
+        axis_offset:0
+    },
+    image:{
+        location: './circle.png'
+    }
+}
